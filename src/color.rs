7a025531bcfceb533f29dc25a38128eee1bdcbdc
Agustin A. <agustinangulo562@gmail.com>
2026-03-04 17:14:02 -0800

    feat: initial commit with support for dithering images using Floyd Steinberg algorithm

---
+++ src/color.rs
@@ -0,0 +1,59 @@
+// Ignore alpha channel
+#[derive(Debug, PartialEq, Clone)]
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+impl Color {
+    pub fn new(r: u8, g: u8, b: u8) -> Color {
+        Color { r, g, b }
+    }
+}
+
+// Technically computes difference^2 instead of just the difference. But this is simplified
+// because we don't care about the actual difference and do care about speed
+pub fn color_difference_rgb(color1: &Color, color2: &Color) -> i32 {
+    let difference = (color1.r as i32 - color2.r as i32).pow(2)
+        + (color1.g as i32 - color2.g as i32).pow(2)
+        + (color1.b as i32 - color2.b as i32).pow(2);
+    difference
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn color_diff_same_color() {
+        let x = Color {
+            r: 255,
+            g: 255,
+            b: 255,
+        };
+        let y = Color {
+            r: 255,
+            g: 255,
+            b: 255,
+        };
+
+        assert_eq!(color_difference_rgb(&x, &y), 0);
+    }
+
+    #[test]
+    fn color_diff_different_color() {
+        let x = Color {
+            r: 255,
+            g: 255,
+            b: 255,
+        };
+        let y = Color {
+            r: 68,
+            g: 133,
+            b: 136,
+        };
+
+        assert_eq!(color_difference_rgb(&x, &y), 64014);
+    }
+}
---
+++ src/dither.rs
@@ -0,0 +1,222 @@
+// # DITHERING
+use crate::color::{Color, color_difference_rgb};
+use image::{ImageBuffer, Rgba};
+
+pub enum DitherMethod {
+    // Error diffusion dithering
+    FloydSteinberg,
+    Atkinson,
+    JarvisJudiceNinke,
+    Stucki,
+    Burkes,
+    Sierra,
+    TwoRowSierra,
+    SierraLite,
+    // Ordered dithering
+    Bayer8,
+    Bayer4,
+    Bayer2,
+    // Misc dithering
+    Threshold,
+    Random,
+}
+
+pub fn find_closest_palette_color(color: Color, palette: &[Color]) -> &Color {
+    // A palette isn't a palette if it's empty. A dithering palette should be at least 2 colors.
+    if palette.len() < 1 {
+        panic!("A color palette cannot be empty.")
+    }
+
+    palette
+        .iter()
+        .min_by_key(|&palette_color| color_difference_rgb(&color, palette_color))
+        .unwrap()
+}
+
+pub fn dither(
+    img_buffer: &mut ImageBuffer<Rgba<u8>, Vec<u8>>,
+    width: i32,
+    height: i32,
+    palette: &[Color],
+    dither_method: DitherMethod,
+) {
+    match dither_method {
+        // Error diffusion dithering
+        DitherMethod::FloydSteinberg
+        | DitherMethod::Atkinson
+        | DitherMethod::JarvisJudiceNinke
+        | DitherMethod::Stucki
+        | DitherMethod::Burkes
+        | DitherMethod::Sierra
+        | DitherMethod::TwoRowSierra
+        | DitherMethod::SierraLite => {
+            error_diffusion_dither(img_buffer, width, height, palette, dither_method)
+        }
+        // Ordered dithering
+        DitherMethod::Bayer2 | DitherMethod::Bayer4 | DitherMethod::Bayer8 => todo!(),
+        // Misc dithering
+        DitherMethod::Threshold => todo!(),
+        DitherMethod::Random => todo!(),
+    }
+}
+
+// ## ERROR DIFFUSION DITHERING
+
+// f32 because our error diffusion calculations use floating point math.
+pub struct QuantError {
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
+}
+
+#[inline]
+fn distribute_error(pixel: &mut Rgba<u8>, err: &QuantError, coeff: f32) {
+    pixel[0] = (pixel[0] as f32 + err.r * coeff).round().clamp(0.0, 255.0) as u8;
+    pixel[1] = (pixel[1] as f32 + err.g * coeff).round().clamp(0.0, 255.0) as u8;
+    pixel[2] = (pixel[2] as f32 + err.b * coeff).round().clamp(0.0, 255.0) as u8;
+}
+
+// ### ERROR DIFFUSION KERNELS
+// For simplicity, order kernels top -> bottom, left -> right.
+
+struct KernelElement {
+    dx: i32,
+    dy: i32,
+    coefficient: f32,
+}
+
+const FLOYD_STEINBERG: [KernelElement; 4] = [
+    KernelElement {
+        dx: 1,
+        dy: 0,
+        coefficient: 7.0 / 16.0,
+    },
+    KernelElement {
+        dx: -1,
+        dy: 1,
+        coefficient: 3.0 / 16.0,
+    },
+    KernelElement {
+        dx: 0,
+        dy: 1,
+        coefficient: 5.0 / 16.0,
+    },
+    KernelElement {
+        dx: 1,
+        dy: 1,
+        coefficient: 1.0 / 16.0,
+    },
+];
+
+fn error_diffusion_dither(
+    img_buffer: &mut ImageBuffer<Rgba<u8>, Vec<u8>>,
+    width: i32,
+    height: i32,
+    palette: &[Color],
+    dither_method: DitherMethod,
+) {
+    let kernel = match dither_method {
+        DitherMethod::FloydSteinberg => FLOYD_STEINBERG,
+        _ => return, // If this ever gets here just die I guess?
+    };
+
+    for cy in 0..height {
+        for cx in 0..width {
+            // An Rgba pixel is a tuple (r, g, b, a).
+            let curr_pixel = img_buffer.get_pixel_mut(cx as u32, cy as u32);
+
+            let new_color = find_closest_palette_color(
+                Color::new(curr_pixel[0], curr_pixel[1], curr_pixel[2]),
+                &palette,
+            );
+
+            let quant_error = QuantError {
+                r: curr_pixel[0] as f32 - new_color.r as f32,
+                g: curr_pixel[1] as f32 - new_color.g as f32,
+                b: curr_pixel[2] as f32 - new_color.b as f32,
+            };
+
+            // Update new pixel
+            // *Note: Only update RGB values, alpha channel is preserved as-is
+            // May change in the future so that any alpha > 0 is changed  to 255 to prevent colors outside target palette
+            curr_pixel[0] = new_color.r;
+            curr_pixel[1] = new_color.g;
+            curr_pixel[2] = new_color.b;
+
+            for ke in &kernel {
+                // Bounds checking: Ensure the pixels we are checking actually exist
+                if (cx + ke.dx) < 0
+                    || (cx + ke.dx) >= width
+                    || (cy + ke.dy) < 0
+                    || (cy + ke.dy) >= height
+                {
+                    continue;
+                }
+                let neighbor = img_buffer.get_pixel_mut((cx + ke.dx) as u32, (cy + ke.dy) as u32);
+                distribute_error(neighbor, &quant_error, ke.coefficient);
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const PALETTE: [Color; 8] = [
+        Color {
+            r: 32,
+            g: 26,
+            b: 11,
+        },
+        Color {
+            r: 252,
+            g: 250,
+            b: 226,
+        },
+        Color {
+            r: 73,
+            g: 14,
+            b: 16,
+        },
+        Color {
+            r: 169,
+            g: 34,
+            b: 15,
+        },
+        Color {
+            r: 43,
+            g: 52,
+            b: 124,
+        },
+        Color {
+            r: 244,
+            g: 179,
+            b: 138,
+        },
+        Color {
+            r: 252,
+            g: 231,
+            b: 110,
+        },
+        Color {
+            r: 43,
+            g: 116,
+            b: 9,
+        },
+    ];
+
+    #[test]
+    fn closest_color_same_color_in_palette() {
+        let x = Color::new(73, 14, 16);
+        let closest_color = find_closest_palette_color(x.clone(), &PALETTE);
+        assert_eq!(x, *closest_color);
+    }
+
+    #[test]
+    fn closest_color_color_not_in_palette() {
+        let x = Color::new(253, 251, 227); // PALETTE[1] with each channel + 1
+        let closest_color = find_closest_palette_color(x, &PALETTE);
+        assert_eq!(Color::new(252, 250, 226), *closest_color);
+    }
+}
